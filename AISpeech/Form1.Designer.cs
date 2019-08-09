47ee1c557ad84079151e5461b8d50eedaec89183
yzh <867609570@qq.com>
2019-08-09 13:53:37 +0800

    添加项目文件。

---
+++ AISpeech/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace AISpeech
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.filePath = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.videoType = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.voiceResult = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // filePath
+            // 
+            this.filePath.Location = new System.Drawing.Point(29, 42);
+            this.filePath.Name = "filePath";
+            this.filePath.Size = new System.Drawing.Size(201, 21);
+            this.filePath.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(236, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "选择文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "音频格式";
+            // 
+            // videoType
+            // 
+            this.videoType.FormattingEnabled = true;
+            this.videoType.Location = new System.Drawing.Point(109, 90);
+            this.videoType.Name = "videoType";
+            this.videoType.Size = new System.Drawing.Size(121, 20);
+            this.videoType.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(236, 88);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "开始识别";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "识别结果";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.voiceResult);
+            this.groupBox1.Controls.Add(this.videoType);
+            this.groupBox1.Controls.Add(this.filePath);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(40, 51);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(343, 333);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "语音识别";
+            // 
+            // voiceResult
+            // 
+            this.voiceResult.Location = new System.Drawing.Point(29, 167);
+            this.voiceResult.Multiline = true;
+            this.voiceResult.Name = "voiceResult";
+            this.voiceResult.Size = new System.Drawing.Size(282, 138);
+            this.voiceResult.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox filePath;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox videoType;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox voiceResult;
+    }
+}
+
---
+++ AISpeech/Form1.cs
@@ -0,0 +1,95 @@
+﻿using Baidu.Aip.Speech;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AISpeech
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //选择文件按钮
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.Title = "C# Corner Open File Dialog";
+            //fdlg.InitialDirectory = @"c:/";   //@是取消转义字符的意思
+            //fdlg.Filter = "All files（*.*）|*.*|All files(*.*)|*.* ";
+            ///*
+            // * FilterIndex 属性用于选择了何种文件类型,缺省设置为0,系统取Filter属性设置第一项
+            // * ,相当于FilterIndex 属性设置为1.如果你编了3个文件类型，当FilterIndex ＝2时是指第2个.
+            // */
+            fdlg.FilterIndex = 2;
+            ///*
+            // *如果值为false，那么下一次选择文件的初始目录是上一次你选择的那个目录，
+            // *不固定；如果值为true，每次打开这个对话框初始目录不随你的选择而改变，是固定的  
+            // */
+            //fdlg.RestoreDirectory = true;
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                //textBox1.Text = System.IO.Path.GetFileNameWithoutExtension(fdlg.FileName);
+                filePath.Text = Path.GetFullPath(fdlg.FileName);
+
+                videoType.Text = Path.GetExtension(fdlg.FileName);//后缀名(视频类型)
+            }
+        }
+        /// <summary>
+        /// 开始识别按钮
+        /// 
+        /// dev_pid 可选参数
+        /// 1536    普通话(支持简单的英文识别)  搜索模型 无标点 支持自定义词库 http://vop.baidu.com/server_api
+        /// 1537    普通话(纯中文识别)  输入法模型 有标点 不支持自定义词库 http://vop.baidu.com/server_api
+        /// 1737    英语 无标点 不支持自定义词库 http://vop.baidu.com/server_api
+        /// 1637    粤语 有标点 不支持自定义词库 http://vop.baidu.com/server_api
+        /// 1837    四川话 有标点 不支持自定义词库 http://vop.baidu.com/server_api
+        /// 1936    普通话远场 远场模型    有标点 不支持自定义词库    http://vop.baidu.com/server_api
+        /// 80001语音识别极速版(收费)
+        /// 80001   普通话 极速版输入法模型    有标点 支持自定义词库 http://vop.baidu.com/pro_api
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var type = videoType.Text;
+            var filePath = this.filePath.Text;
+
+            //设置语音识别应用的账号信息(百度智能云管理中心) APP_ID/API_KEY/SECRET_KEY
+            //demo里的账号
+            //var APP_ID = "14433392";
+            //var API_KEY = "C7WMYgLeWv3Wm2yogwv5gD08";
+            //var SECRET_KEY = "xcvwiwikALBDBaIcGisNQ6aQImtj3qua";
+            //请更改成自己的账号
+            string APP_ID = "16982575";
+            string API_KEY = "0kC4dDwWl3hqo2xz2c4113ZP";
+            string SECRET_KEY = "8aneCyn9KVAWjGKAIfpcr3vMCFt19kIb";
+
+            var client = new Asr(APP_ID, API_KEY, SECRET_KEY);
+            client.Timeout = 120000; // 若语音较长，建议设置更大的超时时间. ms
+
+            // 可选参数
+            var options = new Dictionary<string, object>
+                 {
+                    {"dev_pid", 1536}
+                 };
+
+            var result = client.Recognize(File.ReadAllBytes(filePath), type.TrimStart('.'), 16000, options);
+
+            voiceResult.Text = Convert.ToString(result);
+            result.TryGetValue("result", out JToken resultStr);
+            if (Convert.ToString(resultStr) != "")
+                voiceResult.Text = Convert.ToString(resultStr);
+        }
+    }
+}
